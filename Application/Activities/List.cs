d1a147ef8be019781857ad22965c7af5a30af39e
Himansh2311 <himanshugupta254015@gmail.com>
2021-06-15 15:36:59 +0530

    Error Handling Section

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -12,8 +13,8 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> { }
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -21,22 +22,22 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try
-                {
-                    for (var i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
+                // try
+                // {
+                //     for (var i = 0; i < 10; i++)
+                //     {
+                //         cancellationToken.ThrowIfCancellationRequested();
+                //         await Task.Delay(1000, cancellationToken);
                         
-                    }
-                }
-                catch (Exception ex) when (ex is TaskCanceledException)
-                {
+                //     }
+                // }
+                // catch (Exception ex) when (ex is TaskCanceledException)
+                // {
                      
-                }
-                return await _context.Activities.ToListAsync(cancellationToken);
+                // }
+                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(cancellationToken));
             }
         }
     }
